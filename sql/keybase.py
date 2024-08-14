4d5e0110de5a346b38996df9b0c86ec47be056b5
LI SHIHAO <74440145+Hikiaya@users.noreply.github.com>
2024-08-14 11:24:44 +0800

    变更

--- sql/keybase.py
+++ sql/keybase.py
@@ -31,17 +31,29 @@ class Keys:
         self.__statement = (self.__id, self.__token, self.__key)
         con = sqlite3.connect('Keys.db')
         cur = con.cursor()
-        cur.execute("INSERT INTO Keys(hash,token,key) VALUES(%s,%s,%s)",self.__statement)
+        cur.execute("INSERT INTO Keys(hash,token,key) VALUES(?,?,?)",self.__statement)
         con.commit()
         self.__statement = None
         print('Keys updated')
 
     def __token_metas(self, Hash: str):
+        """
+        使用固定长度的hash值进行查找
+        """
         self.__statement = Hash
         con = sqlite3.connect('Keys.db')
         cur = con.cursor()
-        cur.execute("SELECT key from Keys where hash=" % self.__statement)
+        cur.execute("SELECT key from Keys where hash=?",(self.__statement,))
         self.__statement = None
+
+    def __token_delete(self, key: str):
+        self.__statement = key
+        con = sqlite3.connect('Keys.db')
+        cur = con.cursor()
+        cur.execute("DELETE  from Keys where key=?",(self.__statement,))
+        self.__statement = None
+
+
 
     def __id_sha256(self):
         token_hash = hashlib.sha256()
